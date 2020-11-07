2125f937fd7d82822320de8d0b8f771f71b351b4
Gabe C <coding1729@gmail.com>
2020-11-06 17:23:10 -0700

    1.16/move1.15to1.16 (#5)
    * Update dependencies
    * Port all features from dev

--- src/main/java/com/fractalsmp/fractal_carpet_addon/mixins/EndSpikeFeature_generationMixin.java
+++ src/main/java/com/fractalsmp/fractal_carpet_addon/mixins/EndSpikeFeature_generationMixin.java
@@ -1,11 +1,11 @@
 package com.fractalsmp.fractal_carpet_addon.mixins;
 
 import com.fractalsmp.fractal_carpet_addon.FractalSimpleSettings;
-import com.mojang.datafixers.Dynamic;
+import com.mojang.serialization.Codec;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.IWorld;
+import net.minecraft.world.ServerWorldAccess;
+import net.minecraft.world.StructureWorldAccess;
 import net.minecraft.world.gen.chunk.ChunkGenerator;
-import net.minecraft.world.gen.chunk.ChunkGeneratorConfig;
 import net.minecraft.world.gen.feature.EndSpikeFeature;
 import net.minecraft.world.gen.feature.EndSpikeFeatureConfig;
 import net.minecraft.world.gen.feature.Feature;
@@ -16,23 +16,22 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Random;
-import java.util.function.Function;
 
 @Mixin(EndSpikeFeature.class)
 public abstract class EndSpikeFeature_generationMixin extends Feature<EndSpikeFeatureConfig> {
-    public EndSpikeFeature_generationMixin(Function<Dynamic<?>, ? extends EndSpikeFeatureConfig> configDeserializer) {
-        super(configDeserializer);
+    public EndSpikeFeature_generationMixin(Codec<EndSpikeFeatureConfig> configCodec) {
+        super(configCodec);
     }
 
     @Inject(method="generate", at = @At("HEAD"), cancellable = true)
-    public void suppressGenerate(IWorld iWorld, ChunkGenerator<? extends ChunkGeneratorConfig> chunkGenerator, Random random, BlockPos blockPos, EndSpikeFeatureConfig endSpikeFeatureConfig, CallbackInfoReturnable<Boolean> ci) {
+    public void suppressGenerate(StructureWorldAccess structureWorldAccess, ChunkGenerator chunkGenerator, Random random, BlockPos blockPos, EndSpikeFeatureConfig endSpikeFeatureConfig, CallbackInfoReturnable<Boolean> cir) {
         if (!FractalSimpleSettings.endMainIslandStructureGen) {
-            ci.setReturnValue(true);
+            cir.setReturnValue(true);
         }
     }
 
     @Inject(method="generateSpike", at=@At("HEAD"), cancellable = true)
-    public void suppressGenerateSpike(IWorld world, Random random, EndSpikeFeatureConfig config, EndSpikeFeature.Spike spike, CallbackInfo ci) {
+    public void suppressGenerateSpike(ServerWorldAccess world, Random random, EndSpikeFeatureConfig config, EndSpikeFeature.Spike spike, CallbackInfo ci) {
         if (!FractalSimpleSettings.endMainIslandStructureGen) {
             ci.cancel();
         }
